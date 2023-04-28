481b26f1cd7e5370d7ac1d4097095eeb530ca9d4
AliAkhgari <amin.akhgari@yahoo.com>
2023-04-28 19:28:23 +0330

    chore: Move header to views dir

--- CA5/front/src/App.js
+++ CA5/front/src/App.js
@@ -3,7 +3,7 @@ import Login from './views/login.js'
 import "react-toastify/dist/ReactToastify.css";
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Header from "./components/header/header.js";
+import Header from "./views/header.js";
 import Home from "./views/home.js";
 import Logout from "./views/logout.js";
 
