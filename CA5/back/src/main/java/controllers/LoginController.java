1c5e0f8391f38a09c1e8ea0bd1271835b2f2f05e
AliAkhgari <amin.akhgari@yahoo.com>
2023-04-28 12:38:31 +0330

    feat: Add CrossOrigin

--- CA5/back/src/main/java/controllers/LoginController.java
+++ CA5/back/src/main/java/controllers/LoginController.java
@@ -6,14 +6,11 @@ import exceptions.NotExistentUser;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class LoginController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
