ea0b13ad74130dccda7eed7748eedf385c06d746
AliAkhgari <amin.akhgari@yahoo.com>
2023-05-06 00:57:59 +0330

    feat: Add CrossOrigin

--- CA5/back/src/main/java/controllers/CommentController.java
+++ CA5/back/src/main/java/controllers/CommentController.java
@@ -6,10 +6,12 @@ import exceptions.NotExistentComment;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CommentController {
     @PostMapping(value = "/comment/{id}/like")
