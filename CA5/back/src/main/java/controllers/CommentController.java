120b9b836a8d8a95009f49e8b2a07a9352fb5281
AliAkhgari <amin.akhgari@yahoo.com>
2023-05-06 02:22:31 +0330

    fix: Fix username

--- CA5/back/src/main/java/controllers/CommentController.java
+++ CA5/back/src/main/java/controllers/CommentController.java
@@ -6,20 +6,19 @@ import exceptions.NotExistentComment;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class CommentController {
     @PostMapping(value = "/comment/{id}/like")
-    public ResponseEntity<String> likeComment(@PathVariable String id, HttpSession session) {
+    public ResponseEntity<String> likeComment(@PathVariable String id, @RequestBody Map<String, String> input) {
         int commentId = Integer.parseInt(id);
         try {
             Comment comment = Baloot.getInstance().getCommentById(commentId);
-            String username = (String) session.getAttribute("username");
+            String username = input.get("username");
             comment.addUserVote(username, "like");
             return new ResponseEntity<>("comment liked successfully!", HttpStatus.OK);
         } catch (NotExistentComment e) {
@@ -28,11 +27,11 @@ public class CommentController {
     }
 
     @PostMapping(value = "/comment/{id}/dislike")
-    public ResponseEntity<String> dislikeComment(@PathVariable String id, HttpSession session) {
+    public ResponseEntity<String> dislikeComment(@PathVariable String id, @RequestBody Map<String, String> input) {
         int commentId = Integer.parseInt(id);
         try {
             Comment comment = Baloot.getInstance().getCommentById(commentId);
-            String username = (String) session.getAttribute("username");
+            String username = input.get("username");
             comment.addUserVote(username, "dislike");
             return new ResponseEntity<>("comment liked successfully!", HttpStatus.OK);
         } catch (NotExistentComment e) {
