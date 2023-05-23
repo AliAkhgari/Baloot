babb17e0ac79c929e6dc96941e88b6142f4d4b55
AliAkhgari <amin.akhgari@yahoo.com>
2023-05-23 21:46:17 +0330

    feat: add basic repositories

---
+++ CA6/back/src/main/java/repositories/CommodityRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import entities.Commodity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommodityRepository extends JpaRepository<Commodity, String> {
+}
