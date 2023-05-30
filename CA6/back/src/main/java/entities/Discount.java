4ed4b93c15e4816b3f8eeeb06a6e3487883f188e
AliAkhgari <amin.akhgari@yahoo.com>
2023-05-31 01:17:04 +0330

    feat: use lombok for getters and setters

--- CA6/back/src/main/java/entities/Discount.java
+++ CA6/back/src/main/java/entities/Discount.java
@@ -3,27 +3,18 @@ package entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "discounts")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Discount {
     @Id
     private String discountCode;
+
     private float discount;
-
-    public String getDiscountCode() {
-        return discountCode;
-    }
-
-    public void setDiscountCode(String discountCode) {
-        this.discountCode = discountCode;
-    }
-
-    public float getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(float discount) {
-        this.discount = discount;
-    }
 }
