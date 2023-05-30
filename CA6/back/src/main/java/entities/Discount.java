74ae6bfb912ff774ace2c92d575b321bb75f3564
AliAkhgari <amin.akhgari@yahoo.com>
2023-05-31 01:09:38 +0330

    feat: add discount table

--- CA6/back/src/main/java/entities/Discount.java
+++ CA6/back/src/main/java/entities/Discount.java
@@ -1,6 +1,13 @@
 package entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "discounts")
 public class Discount {
+    @Id
     private String discountCode;
     private float discount;
 
