84c695b4ea4640375599627361473f0208f46f16
AliAkhgari <amin.akhgari@yahoo.com>
2023-05-31 16:53:23 +0330

    feat: add fetch and store discounts data from api

--- CA6/back/src/main/java/SpringApplication/BalootApplication.java
+++ CA6/back/src/main/java/SpringApplication/BalootApplication.java
@@ -1,16 +1,17 @@
 package SpringApplication;
 
-import application.Baloot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "controllers")
+@ComponentScan(basePackages = {"controllers", "services", "repositories"})
+@EnableJpaRepositories(basePackages = "repositories")
+@EntityScan(basePackages = "entities")
 public class BalootApplication {
     public static void main(String[] args) {
-        Baloot.getInstance().fetchAndStoreDataFromAPI();
         SpringApplication.run(BalootApplication.class, args);
-
     }
 }
--- CA6/back/src/main/java/services/DiscountService.java
+++ CA6/back/src/main/java/services/DiscountService.java
@@ -1,21 +1,39 @@
 package services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import entities.Discount;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import repositories.DiscountRepository;
+import utils.Request;
+
+import java.io.IOException;
+import java.util.List;
+
+import static defines.Endpoints.DISCOUNTS_ENDPOINT;
+import static defines.Endpoints.HOST;
 
 @Service
 public class DiscountService {
     private final DiscountRepository discountRepository;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public DiscountService(DiscountRepository discountRepository) {
         this.discountRepository = discountRepository;
+        this.fetchAndSaveDiscountsFromApi();
     }
 
-    public void dummy() {
-        System.out.println("dummy");
+    public void fetchAndSaveDiscountsFromApi() {
+        try {
+            String discountsString = Request.makeGetRequest(HOST + DISCOUNTS_ENDPOINT);
+            List<Discount> discountList = objectMapper.readValue(discountsString, new TypeReference<>() {
+            });
+            discountRepository.saveAll(discountList);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 //    public Discount getDiscountById(String id) {
 //        return this.discountRepository.fin
 //    }
