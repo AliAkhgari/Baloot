3363cb074d7f214ac7fcf381dcdeb97497bfa364
AliAkhgari <amin.akhgari@yahoo.com>
2023-06-03 01:21:59 +0330

    feat: add user table

--- CA6/back/src/main/java/entities/User.java
+++ CA6/back/src/main/java/entities/User.java
@@ -1,29 +1,33 @@
 package entities;
 
-import exceptions.CommodityIsNotInBuyList;
 import exceptions.InsufficientCredit;
 import exceptions.InvalidCreditRange;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
+    @Id
     private String username;
     private String password;
     private String email;
     private String birthDate;
     private String address;
     private float credit;
-    private Map<Integer, Integer> commoditiesRates = new HashMap<>();
-    private Map<String, Integer> buyList = new HashMap<>();
-    private Map<String, Integer> purchasedList = new HashMap<>();
-    private ArrayList<String> usedDiscounts = new ArrayList<>();
-    private Discount currentDiscount = null;
+//    private Map<Integer, Integer> commoditiesRates = new HashMap<>();
+//    private Map<String, Integer> buyList = new HashMap<>();
+//    private Map<String, Integer> purchasedList = new HashMap<>();
+//    private ArrayList<String> usedDiscounts = new ArrayList<>();
+//    private Discount currentDiscount = null;
 
-    public User() {
-
-    }
 
     public User(String username, String password, String email, String birthDate, String address) {
         this.username = username;
@@ -33,66 +37,11 @@ public class User {
         this.address = address;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Map<String, Integer> getBuyList() {
-        return buyList;
-    }
-
-    public void setBuyList(Map<String, Integer> buyList) {
-        this.buyList = buyList;
-    }
-
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public float getCredit() {
-        return this.credit;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setCredit(float credit) {
-        this.credit = credit;
-    }
 
     public void addCredit(float amount) throws InvalidCreditRange {
         if (amount < 0)
             throw new InvalidCreditRange();
-        
+
         this.credit += amount;
     }
 
@@ -102,78 +51,51 @@ public class User {
 
         this.credit -= amount;
     }
-    public Map<Integer, Integer> getCommoditiesRates() {
-        return this.commoditiesRates;
-    }
 
-    public Map<String, Integer> getPurchasedList() {
-        return purchasedList;
-    }
-
-    public void setPurchasedList(Map<String, Integer> purchased_list) {
-        this.purchasedList = purchased_list;
-    }
-
-    public ArrayList<String> getUsedDiscounts() {
-        return usedDiscounts;
-    }
-
-    public void setUsedDiscounts(ArrayList<String> usedDiscounts) {
-        this.usedDiscounts = usedDiscounts;
-    }
-
-    public void addCurrentDiscountToUsed() {
-        if (this.currentDiscount == null)
-            return;
-        this.usedDiscounts.add(this.currentDiscount.getDiscountCode());
-        this.currentDiscount = null;
-    }
-
-    public Discount getCurrentDiscount() {
-        return currentDiscount;
-    }
-
-    public void setCurrentDiscount(Discount currentDiscount) {
-        this.currentDiscount = currentDiscount;
-    }
-
-    public void addRatedCommodities(int commodity_id, int score) {
-        this.commoditiesRates.put(commodity_id, score);
-    }
-
-    public void addBuyItem(Commodity commodity) {
-        String id = commodity.getId();
-        if (this.buyList.containsKey(id)) {
-            int existingQuantity = this.buyList.get(id);
-            this.buyList.put(id, existingQuantity + 1);
-        } else
-            this.buyList.put(id, 1);
-    }
-
-    public void addPurchasedItem(String id, int quantity) {
-        if (this.purchasedList.containsKey(id)) {
-            int existingQuantity = this.purchasedList.get(id);
-            this.purchasedList.put(id, existingQuantity + quantity);
-        } else
-            this.purchasedList.put(id, quantity);
-    }
-
-    public void removeItemFromBuyList(Commodity commodity) throws CommodityIsNotInBuyList {
-        String id = commodity.getId();
-        if (this.buyList.containsKey(id)) {
-            int existingQuantity = this.buyList.get(id);
-            if (existingQuantity == 1)
-                this.buyList.remove(commodity.getId());
-            else
-                this.buyList.put(id, existingQuantity - 1);
-        } else
-            throw new CommodityIsNotInBuyList();
-    }
-
-    public float getCurrentDiscountAmount() {
-        if (this.currentDiscount == null)
-            return 0;
-
-        return this.currentDiscount.getDiscount() / 100;
-    }
+//    public void addCurrentDiscountToUsed() {
+//        if (this.currentDiscount == null)
+//            return;
+//        this.usedDiscounts.add(this.currentDiscount.getDiscountCode());
+//        this.currentDiscount = null;
+//    }
+//
+//    public void addRatedCommodities(int commodity_id, int score) {
+//        this.commoditiesRates.put(commodity_id, score);
+//    }
+//
+//    public void addBuyItem(Commodity commodity) {
+//        String id = commodity.getId();
+//        if (this.buyList.containsKey(id)) {
+//            int existingQuantity = this.buyList.get(id);
+//            this.buyList.put(id, existingQuantity + 1);
+//        } else
+//            this.buyList.put(id, 1);
+//    }
+//
+//    public void addPurchasedItem(String id, int quantity) {
+//        if (this.purchasedList.containsKey(id)) {
+//            int existingQuantity = this.purchasedList.get(id);
+//            this.purchasedList.put(id, existingQuantity + quantity);
+//        } else
+//            this.purchasedList.put(id, quantity);
+//    }
+//
+//    public void removeItemFromBuyList(Commodity commodity) throws CommodityIsNotInBuyList {
+//        String id = commodity.getId();
+//        if (this.buyList.containsKey(id)) {
+//            int existingQuantity = this.buyList.get(id);
+//            if (existingQuantity == 1)
+//                this.buyList.remove(commodity.getId());
+//            else
+//                this.buyList.put(id, existingQuantity - 1);
+//        } else
+//            throw new CommodityIsNotInBuyList();
+//    }
+//
+//    public float getCurrentDiscountAmount() {
+//        if (this.currentDiscount == null)
+//            return 0;
+//
+//        return this.currentDiscount.getDiscount() / 100;
+//    }
 }
---
+++ CA6/back/src/main/java/services/UserService.java
@@ -0,0 +1,42 @@
+package services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import entities.User;
+import exceptions.NotExistentUser;
+import org.springframework.stereotype.Service;
+import repositories.UserRepository;
+import utils.Request;
+
+import java.io.IOException;
+import java.util.List;
+
+import static defines.Endpoints.HOST;
+import static defines.Endpoints.USERS_ENDPOINT;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.fetchAndSaveProvidersFromApi();
+    }
+
+    public void fetchAndSaveProvidersFromApi() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String usersString = Request.makeGetRequest(HOST + USERS_ENDPOINT);
+            List<User> userList = objectMapper.readValue(usersString, new TypeReference<>() {
+            });
+            userRepository.saveAll(userList);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User getUserById(String id) throws NotExistentUser {
+        return userRepository.findById(id)
+                .orElseThrow(NotExistentUser::new);
+    }
+}
