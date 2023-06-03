b72ec3db50ebc117214819eab2eb07def56fc9fe
AliAkhgari <amin.akhgari@yahoo.com>
2023-06-03 18:10:13 +0330

    feat: add getCommentsForCommodity

--- CA6/back/src/main/java/services/CommentService.java
+++ CA6/back/src/main/java/services/CommentService.java
@@ -3,10 +3,8 @@ package services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import entities.Comment;
-import entities.CommentReaction;
 import exceptions.NotExistentComment;
 import org.springframework.stereotype.Service;
-import repositories.CommentReactionRepository;
 import repositories.CommentRepository;
 import utils.Request;
 
@@ -19,53 +17,35 @@ import static defines.Endpoints.HOST;
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
-    private final CommentReactionRepository commentReactionRepository;
 
-    public CommentService(CommentRepository commentRepository, CommentReactionRepository commentReactionRepository) {
+    public CommentService(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
-        this.commentReactionRepository = commentReactionRepository;
         this.fetchAndSaveCommentsFromApi();
     }
 
     public void fetchAndSaveCommentsFromApi() {
         try {
-            System.out.println("1 in fetchAndSaveCommentsFromApi");
-
             ObjectMapper objectMapper = new ObjectMapper();
             String commentsString = Request.makeGetRequest(HOST + COMMENTS_ENDPOINT);
 
-            System.out.println(commentsString);
             List<Comment> commentList = objectMapper.readValue(commentsString, new TypeReference<>() {
             });
 
-            System.out.println("2 in fetchAndSaveCommentsFromApi");
-//
-//            // TODO: setUsername
-//
-            System.out.println(commentList);
-
-            commentRepository.saveAll(commentList);
-            System.out.println("3 in fetchAndSaveCommentsFromApi");
+//          TODO: setUsername
             for (Comment comment : commentList) {
                 comment.setUsername("ff");
-//
             }
+
+            commentRepository.saveAll(commentList);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-//    public void setCommentsUsername() {
-//        List<Comment> comments = commentRepository.findAll();
-//        for (Comment comment : comments) {
-//            User user = userRepository.findByEmail(comment.getUserEmail());
-//            if (user != null) {
-//                comment.setUsername(user.getUsername());
-//                commentRepository.save(comment);
-//            }
-//        }
-//    }
-
+    public List<Comment> getCommentsForCommodity(String commodityId) {
+        return commentRepository.findByCommodityId(commodityId);
+    }
 
     public void addComment(Comment comment) {
         commentRepository.save(comment);
@@ -75,8 +55,4 @@ public class CommentService {
         return commentRepository.findById(id)
                 .orElseThrow(NotExistentComment::new);
     }
-
-//    public void reactComment(String reaction) {
-//        commentReactionRepository.save(new CommentReaction())
-//    }
 }
