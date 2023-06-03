5a6365993e8ad198b4b176c89e41ad76d018eb2f
AliAkhgari <amin.akhgari@yahoo.com>
2023-06-03 20:30:22 +0330

    feat: add user service

--- CA6/back/src/main/java/controllers/UserController.java
+++ CA6/back/src/main/java/controllers/UserController.java
@@ -1,22 +1,28 @@
 package controllers;
 
-import application.Baloot;
 import entities.User;
 import exceptions.InvalidCreditRange;
 import exceptions.NotExistentUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import services.UserService;
 
 import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping(value = "/users/{id}")
     public ResponseEntity<User> getUser(@PathVariable String id) {
         try {
-            User user = Baloot.getInstance().getUserById(id);
+            User user = userService.getUserById(id);
             return new ResponseEntity<>(user, HttpStatus.OK);
         } catch (NotExistentUser e) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -27,7 +33,8 @@ public class UserController {
     public ResponseEntity<String> addCredit(@PathVariable String id, @RequestBody Map<String, String> input) {
         try {
             float credit = Float.parseFloat(input.get("credit"));
-            Baloot.getInstance().getUserById(id).addCredit(credit);
+            User user = userService.getUserById(id);
+            user.addCredit(credit);
             return new ResponseEntity<>("credit added successfully!", HttpStatus.OK);
         } catch (InvalidCreditRange e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
@@ -37,7 +44,5 @@ public class UserController {
             return new ResponseEntity<>("Please enter a valid number for the credit amount."
                     , HttpStatus.BAD_REQUEST);
         }
-
-
     }
 }
