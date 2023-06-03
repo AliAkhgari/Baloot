2436b518f86de30ae8bbd10f9a0b3ea5a6a58ffd
AliAkhgari <amin.akhgari@yahoo.com>
2023-06-04 02:04:21 +0330

    fix: fix Id

--- CA6/back/src/main/java/repositories/UserRatingRepository.java
+++ CA6/back/src/main/java/repositories/UserRatingRepository.java
@@ -1,5 +1,6 @@
 package repositories;
 
+import entities.CommodityUserId;
 import entities.UserRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRatingRepository extends JpaRepository<UserRating, String> {
+public interface UserRatingRepository extends JpaRepository<UserRating, CommodityUserId> {
     @Query("SELECT AVG(u.score) FROM UserRating u WHERE u.commodity.id = :commodityId")
     Float getAverageScoreByCommodityId(@Param("commodityId") String commodityId);
 }
