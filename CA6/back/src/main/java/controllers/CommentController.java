03bd06472c7b62d55388c113d4fe50de8b44d412
AliAkhgari <amin.akhgari@yahoo.com>
2023-06-04 00:07:16 +0330

    feat: add comment reaction db

--- CA6/back/src/main/java/controllers/CommentController.java
+++ CA6/back/src/main/java/controllers/CommentController.java
@@ -5,6 +5,7 @@ import exceptions.NotExistentComment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import services.CommentReactionService;
 import services.CommentService;
 
 import java.util.Map;
@@ -13,16 +14,18 @@ import java.util.Map;
 @RestController
 public class CommentController {
     private final CommentService commentService;
+    private final CommentReactionService commentReactionService;
 
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, CommentReactionService commentReactionService) {
         this.commentService = commentService;
+        this.commentReactionService = commentReactionService;
     }
 
     @PostMapping(value = "/comment/{id}/like")
     public ResponseEntity<String> likeComment(@PathVariable String id, @RequestBody Map<String, String> input) {
         try {
             Comment comment = commentService.getCommentById(id);
-            comment.addUserVote("like");
+            commentReactionService.addReaction(comment, "like");
             return new ResponseEntity<>("comment liked successfully!", HttpStatus.OK);
         } catch (NotExistentComment e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
@@ -33,8 +36,8 @@ public class CommentController {
     public ResponseEntity<String> dislikeComment(@PathVariable String id, @RequestBody Map<String, String> input) {
         try {
             Comment comment = commentService.getCommentById(id);
-            comment.addUserVote("dislike");
-            return new ResponseEntity<>("comment liked successfully!", HttpStatus.OK);
+            commentReactionService.addReaction(comment, "dislike");
+            return new ResponseEntity<>("comment disliked successfully!", HttpStatus.OK);
         } catch (NotExistentComment e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
