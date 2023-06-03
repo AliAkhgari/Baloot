88747d1aaed2c5b23d75d721e13a2ae8d28dabf5
AliAkhgari <amin.akhgari@yahoo.com>
2023-06-04 00:07:37 +0330

    fix: fix id

--- CA6/back/src/main/java/repositories/CommentReactionRepository.java
+++ CA6/back/src/main/java/repositories/CommentReactionRepository.java
@@ -1,9 +1,10 @@
 package repositories;
 
 import entities.CommentReaction;
+import entities.CommodityUserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentReactionRepository extends JpaRepository<CommentReaction, String> {
+public interface CommentReactionRepository extends JpaRepository<CommentReaction, CommodityUserId> {
 }
